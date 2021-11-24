008078ff7f7e68129184f826bb2ebd2c2be9f114
Christopher Luvumbo <christopherluvumbo@gmail.com>
2021-11-24 17:31:34 +0100

    add template

---
+++ client/src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+import Title from "./Header/Title"
+
+const Header = () => {
+	return (
+		<>
+        <nav>
+            <Title></Title>
+        </nav>
+        </>
+	);
+};
+
+export default Header;
